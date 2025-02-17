080902812fae4c352f172d7d56da472c59b60eef
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2025-02-17 16:40:51 +0300

    first version

---
+++ DegreesNumber/Program.fs
@@ -0,0 +1,12 @@
+﻿let exponentiation first second = 
+    let rec addList list counter = 
+        if counter <= second + first then
+            if List.length list = 0 then
+                addList (pown 2 counter::list) (counter + 1)
+            else
+                addList (List.item 0 list * 2::list) (counter + 1)
+        else
+            List.rev list
+    addList [] first
+
+printfn "%A" (exponentiation 8 4)
