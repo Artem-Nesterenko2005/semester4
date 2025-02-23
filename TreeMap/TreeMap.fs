a2336607a4d701913ce003def3624879ab6852fb
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2025-02-23 15:43:54 +0300

    first version

---
+++ TreeMap/TreeMap.fs
@@ -0,0 +1,12 @@
+﻿module TreeMap
+    // A binary tree data structure.
+    type 'a Tree = 
+    | Leaf of 'a
+    | Node of 'a * 'a Tree * 'a Tree
+
+    // A function that applies a function to each element of the tree, returning a new tree.
+    let rec map func tree =
+        match tree with
+        | Leaf(data) -> Leaf(func(data))
+        | Node(data, left, right) -> 
+            Node(func(data), map func left, map func right)
