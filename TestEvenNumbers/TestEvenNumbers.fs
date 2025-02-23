10f752a70e199ead7ce0faa890a1d049ab390adc
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2025-02-23 15:22:14 +0300

    first version

---
+++ TestEvenNumbers/TestEvenNumbers.fs
@@ -0,0 +1,27 @@
+﻿module TestEvenNumbers
+
+open NUnit.Framework
+open FsUnit
+open EvenNumbers
+open FsCheck
+
+[<TestFixture>]
+module Tests =
+
+    [<Test>]
+    let ``MapEvenNumbers usual test`` () = 
+        mapEvenNumbers([1; 5; -2; 4; -9; 3]) |> should equal 2
+
+    [<Test>]
+    let ``FilterEvenNumbers usual test``() = 
+        filterEvenNumbers([3; 4; -11; -2; 5; 1]) |> should equal 2
+
+    [<Test>]
+    let ``FoldEvenNumbers usual test``() = 
+        foldEvenNumbers([-6; 7; 2; 1; -5; 9]) |> should equal 2
+
+    [<Test>]
+    let ``FsCheck check functions``() =
+        let comparisonFunctions list =
+            (mapEvenNumbers list = filterEvenNumbers list) && (filterEvenNumbers list = foldEvenNumbers list)
+        Check.QuickThrowOnFailure comparisonFunctions
